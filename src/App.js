2692b922552522fded4695fb867391ab3330c2a4
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-05 15:03:22 -0500

    add my react template

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/// external modules ///
+import React from 'react';
+
+/// styles ///
+import './styles/App.css';
+
+/***************************************
+  COMPONENT
+***************************************/
+function App () {
+  return (
+    <div className='App'>
+    </div>
+  );
+}
+
+/**************************************/
+export default App;
