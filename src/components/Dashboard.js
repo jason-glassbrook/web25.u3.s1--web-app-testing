a140a7c4da4efdcd481d6b111b08bd9f5258a239
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-05 16:57:20 -0500

    add skeletons for Dashboard, Display

---
+++ src/components/Dashboard.js
@@ -0,0 +1,19 @@
+/// external modules ///
+import React from 'react';
+
+/***************************************
+  STATES
+***************************************/
+const init = {};
+
+/***************************************
+  COMPONENT
+***************************************/
+const Dashboard = (props) => {
+  return (
+    <div className='Dashboard'>Dashboard</div>
+  );
+};
+
+/**************************************/
+export default Dashboard;
---
+++ src/components/Display.js
@@ -0,0 +1,19 @@
+/// external modules ///
+import React from 'react';
+
+/***************************************
+  STATES
+***************************************/
+const init = {};
+
+/***************************************
+  COMPONENT
+***************************************/
+const Display = (props) => {
+  return (
+    <div className='Display'>Display</div>
+  );
+};
+
+/**************************************/
+export default Display;
