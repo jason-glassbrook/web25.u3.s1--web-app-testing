47bdc72ee2003357d1b8f1abbb10336697f80dd6
o1123516766542617 <o1123516766542617@outlook.com>
2019-12-05 19:29:56 -0500

    add FlexCol helper component

---
+++ src/components/FlexCol.js
@@ -0,0 +1,20 @@
+/// external modules ///
+import styled from 'styled-components';
+
+/***************************************
+  COMPONENT
+***************************************/
+const FlexCol = styled.div `
+  display: flex;
+  flex-flow: row nowrap;
+  align-content: center;
+  justify-content: space-around;
+  align-items: stretch;
+`;
+
+/**************************************/
+export default FlexCol;
+
+export {
+  FlexCol,
+};
